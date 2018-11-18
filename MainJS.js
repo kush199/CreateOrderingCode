daab70b3919871d21db65a655f85d509f902ed29
kumar shubhendu <livelifekush@gmail.com>
2018-11-18 12:34:23 +0530

    added js file

---
+++ MainJS.js
@@ -0,0 +1,11 @@
+﻿var pattern = "#1-#2-#3-#4-#5-#6-#7";
+function TypeChange() {
+	pattern = pattern.replace("#1", document.getElementById("Type").value)
+	pattern = pattern.replace("#2","")
+	pattern = pattern.replace("#3", "")
+	pattern = pattern.replace("#4", "")
+	pattern = pattern.replace("#5", "")
+	pattern = pattern.replace("#6", "")
+	pattern = pattern.replace("#7", "")
+	document.getElementById("OrderingCodeId").value = pattern;
+}
